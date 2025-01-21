6bd1ef832bcd540edcd86825b4b614d52491caea
dimabalushcka@gmail.com <dimabalushcka@gmail.com>
2025-01-21 12:52:57 +0200

    add files

--- UART comm/UART communication.py
+++ UART comm/UART communication.py
@@ -1,76 +1,56 @@
-import sys
-import random
-import string
-from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget
+import serial
 
+ser = serial.Serial('COM4', 38400, timeout=1)  # Змініть COM4 на свій порт
 
+def write_flash(data):
 
-#1
-class WelcomeScreen(QDialog):
-    def init(self):
-        super(WelcomeScreen, self).init()
-        loadUi(r"C:\Users\ONLINE\Desktop\1\welcomescreen.ui", self)
-        self.arrow.clicked.connect(self.gotologin)
-        # до генерації
-        self.pushButton.clicked.connect(self.generate)
+    command = f"WRITE_FLASH {data}\r"
+    ser.write(command.encode())
+    response = ser.readline().decode().strip()
+    print(f"STM32 відповів: {response}")
 
-    def gotologin(self):
-        secondscreen = PasswordScreen()
-        widget.addWidget(secondscreen)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+def read_flash():
+    """
+    Запитує дані з флеш-пам'яті STM32.
+    """
+    ser.write("READ_FLASH\r".encode())
+    print("Отримання даних з флеш-пам'яті...")
 
-#генерація випадкового паролю
-    def generate(self):
-        password = "".join(random.choices(string.ascii_letters + string.digits, k =10))   # string.punctuation розділові знаки
-        self.lineEdit.setText(password)
+    result = ""
+    while True:
+        packet = ser.read(64)
+        if not packet:
+            break
+        result += packet.decode(errors='ignore')
 
+    print(f"Отримані дані: {result.strip()}")
 
-#2
-class PasswordScreen(QDialog):
-    def init(self):
-        super(PasswordScreen, self).init()
-        loadUi("C:/Users/ONLINE/Desktop/1/secondscreen.ui", self)
-        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.sendbutton.clicked.connect(self.send)
+def main():
+    """
+    Основна функція для взаємодії з STM32.
+    """
+    try:
+        while True:
+            print("\nМеню:")
+            print("1. Записати дані у флеш (WRITE_FLASH)")
+            print("2. Прочитати дані з флеш (READ_FLASH)")
+            print("3. Вийти")
+            choice = input("Ваш вибір: ")
 
-    def send(self):
-        true = TrueScreen()
-        widget.addWidget(true)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+            if choice == "1":
+                data = input("Введіть дані для запису: ")
+                write_flash(data)
+            elif choice == "2":
+                read_flash()
+            elif choice == "3":
+                print("Програма завершена.")
+                break
+            else:
+                print("Невірний вибір. Спробуйте ще раз.")
+    except KeyboardInterrupt:
+        print("\nПрограма зупинена.")
+    finally:
+        ser.close()
 
-#3
-class TrueScreen(QDialog):
-    def init(self):
-        super(TrueScreen, self).init()
-        loadUi("C:/Users/ONLINE/Desktop/1/true.ui", self)
-
-    def getdata(self):
-        lastscreen = LastScreen()
-        widget.addWidget(lastscreen)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-
-
-#4
-class LastScreen(QDialog):
-    def init(self):
-        super(LastScreen, self).init()
-        loadUi( "C:/Users/ONLINE/Desktop/1/lastscreen.ui" ,self)
-
-
-
-
-#main
-app = QApplication(sys.argv)
-welcome=WelcomeScreen()
-widget = QStackedWidget()
-widget.addWidget(welcome)
-widget.setFixedHeight(780)
-widget.setFixedWidth(1024)
-widget.show()
-try:
-    sys.exit(app.exec_())
-except:
-    print("Exiting")
+if __name__ == "__main__":
+    main()
